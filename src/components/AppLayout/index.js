83db16734963d6de0d2a8087af8f0a183d2a1915
AJ <ajcatarina@outlook.com>
2019-05-11 21:05:54 +0800

    Add initial react app setup

---
+++ src/components/AppLayout/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppLayout(props) {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default AppLayout
---
+++ src/components/AppView/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppView(props) {
+  return (
+    <>
+      <AppLayout />
+    </>
+  )
+}
+
+export default AppView

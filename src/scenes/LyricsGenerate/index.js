680ea330cdcf6a0c98b339109a8118078ef03d7a
AJ <ajcatarina@outlook.com>
2019-08-05 04:34:56 +0800

    Save progress on customizing PWA install

--- src/scenes/LyricsGenerate/index.js
+++ src/scenes/LyricsGenerate/index.js
@@ -84,6 +84,34 @@ function LyricsGenerate(props) {
     setImages(images)
   }, [debouncedLyrics, debouncedSections])
 
+  const btnAdd = document.getElementById('install')
+  let how
+  var deferredPrompt
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    console.log('hello')
+    how = () => console.log('how')
+    // Update UI notify the user they can add to home screen
+    btnAdd.style.display = 'block';
+  });
+
+  const hey = () => {
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  }
+
   return (
     <>
       <div style={{ padding: 10 }}>
@@ -97,6 +125,21 @@ function LyricsGenerate(props) {
           <Typography style={{ fontFamily: 'Quicksand' }} gutterBottom>
             Lyrics Generator BETA
           </Typography>
+        </div>
+        <div style={{ paddingTop: 5 }}>
+          <ButtonBase 
+            id="install"
+            onClick={how}
+            style={{
+              padding: 10,
+              backgroundColor: ready ? 'black' : 'gray',
+              color: 'white',
+              float: 'right ',
+              display: 'none'
+            }}
+          >
+            INSTALL
+          </ButtonBase>
         </div>
         <div style={{ paddingTop: 5 }}>
           <ButtonBase component="a" download={`${title}.zip`} href={zipHref}
